ce4ce556243d52cd3fbb03129006343e89df027d
Medrouad <med.rouad@gmail.com>
2018-01-10 22:59:55 +0100

    Add files via upload

---
+++ TestDataService/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace TestDataService
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string heure = "10";
+            DateTime date = Outils.convertirStringToDateTime("14/04/2017 21:08:44");
+            string res = Outils.fusion(date, heure);
+            Console.WriteLine(res);
+
+            Vehicule v = ControleurVehicule.getVehiculeDispo(heure, date);
+            if (v == null)
+            {
+                Console.WriteLine("null");
+            }
+            
+        }
+    }
+}
+
